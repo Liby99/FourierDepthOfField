ddb76177dcf897adfc0751592deeeb94ed3f6353
Liby99 <liby99@icloud.com>
2018-02-05 00:28:17 -0800

    Setup depth and test

---
+++ test/depth.cpp
@@ -0,0 +1,21 @@
+using namespace fdof;
+
+int main() {
+    
+    /*
+    DepthTracer dt;
+    dt.setResolution(64, 48);
+    
+    Scene scn;
+    scn.setBackgroundColor(Color(0.3f, 0.3f, 0.8f));
+    scn.getCamera().setPosition(0.0f, 2.0f, 2.0f);
+    
+    Lambert lbt(Color(1.0f, 1.0f, 1.0f));
+    Cube c(1.0f);
+    c.setMaterial(lbt);
+    scn.addObject(c);
+    
+    Bitmap bmp = dt.render(scn);
+    bmp.save("depthmap.bmp");
+    */
+}
