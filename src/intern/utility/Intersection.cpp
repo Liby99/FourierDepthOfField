69dbdfd38dc421b52d99c52999e947f7515f3716
Liby Lee <liby99@icloud.com>
2018-02-21 11:30:27 -0800

    Fixed Intersection T

--- src/intern/utility/Intersection.cpp
+++ src/intern/utility/Intersection.cpp
@@ -44,4 +44,5 @@ void Intersection::transform(mat4 transf) {
     vec4 nNorm = transpose(inverse(transf)) * vec4(normal, 0);
     position = vec3(nPos) / nPos.w;
     normal = vec3(nNorm);
+    t = distToOrigin();
 }
