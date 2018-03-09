f6395d6d26b67ec144542bd154239deef352448a
Liby Lee <liby99@icloud.com>
2018-03-08 16:51:54 -0800

    Improved Cubes Scene

--- test/cubes.cpp
+++ test/cubes.cpp
@@ -7,13 +7,15 @@ int main() {
     pt.setSuperSampling(25);
     Scene scn;
     scn.background = Color(0.5, 0.6, 0.8);
-    scn.getCamera().setPosition(vec3(0, 0.3, 2));
+    scn.getCamera().setPosition(vec3(-0.1, 0.3, 2));
     scn.getCamera().setFocalDistance(2);
     scn.getCamera().setAperture(0.1);
+    
     DirectLight dl;
     dl.color = Color(0.8, 0.8, 0.8);
+    
     scn.addLight(dl);
-    Lambert lbt(Color(0.3, 0.3, 0.4));
+    Lambert lbt(Color(0.8, 0.8, 0.8));
     
     std::vector<Cube *> cubes;
     for (int i = 0; i < 100; i++) {
@@ -23,6 +25,7 @@ int main() {
               y = Sampler::random() * 0.1 - 0.05,
               z = Sampler::random() * 4 - 2;
         cube->transform.setPosition(vec3(x, y, z));
+        cube->transform.setRotation(vec3(Sampler::random(), Sampler::random(), Sampler::random()));
         scn.addObject(*cube);
         cubes.push_back(cube);
     }
